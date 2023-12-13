f206a741217454dbb6905a46767edabdf8bb7cde
Sayeeduddin <iamsayeeduddin@outlook.com>
2023-12-13 19:25:22 +0530

    2nd Day - JS

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
-// console.log("Hello World!");
+// var age = 10;
+// var name = "Sayeed";
 
-// Variables ->
-// const a = 10;
-// a = 11;
+// Number
+
+// var a = 10;
+// var b = a + 1;
+// a = 5;
+// console.log(b);
+
+// const res = a + b;
+// console.log(res);
+
+// var res = 5 % 2;
+// console.log(res);
+
+// var a = 1;
+// console.log(a--);
+
+// var a = 10;
+// var b = 4;
+// a %= b;
 // console.log(a);
 
-// Data Types
-console.log("HELLLLLLOOOOO!");
+// var a = 10 * (2 + 3);
+// console.log(a);
+
+// var age = 18;
+// console.log(age <= 18);
+
+// AND
+// A  && B
+// T    F   F
+// F    F   F
+// F    T   F
+// T    T   T
+
+// AND
+// A  || B
+// T    F   T
+// F    F   F
+// F    T   T
+// T    T   T
+
+// NOT
+// A !A
+// F T
+// T F
+
+// FALSY VALUES = false, 0,"", null, undefined, NaN
+
+// console.log((true && false) || (true && false && false));
+// var name = prompt("Please Enter the Name of the USer");
+var name = "Sayeed";
+console.log("The Name is: " + name);
